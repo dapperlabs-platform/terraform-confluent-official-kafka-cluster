b9493058727a76a0126e8a254bded9f1983e4964
Vasilii N. Polikarpov <vasilii.polikarpov@dapperlabs.team>
2022-08-24 17:11:39 +0200

    Add ccloud-exporter config

--- ccloud-exporter.tf
+++ ccloud-exporter.tf
@@ -17,6 +17,21 @@ resource "kubernetes_service_account" "ccloud_exporter_service_account" {
   }
 }
 
+
+resource "kubernetes_secret" "ccloud_exporter_config_file" {
+  count = var.enable_metric_exporters ? 1 : 0
+
+  metadata {
+    name      = "${local.ccloud_exporter_name}-config-file"
+    namespace = var.metric_exporters_namespace
+    labels    = local.ccloud_exporter_common_labels
+  }
+
+  data = {
+    "config.yaml" = file("${path.module}/templates/ccloud-exporter.yaml")
+  }
+}
+
 resource "kubernetes_secret" "ccloud_exporter_config" {
   count = var.enable_metric_exporters ? 1 : 0
 
@@ -74,6 +89,8 @@ resource "kubernetes_deployment" "ccloud_exporter_deployment" {
           # Repo recommends pointing to master
           image_pull_policy = "Always"
 
+          command = ["-config", "/opt/docker/conf/config.yaml"]
+
           resources {
             requests = var.ccloud_exporter_container_resources.requests
             limits   = var.ccloud_exporter_container_resources.limits
@@ -83,6 +100,12 @@ resource "kubernetes_deployment" "ccloud_exporter_deployment" {
             secret_ref {
               name = kubernetes_secret.ccloud_exporter_config[0].metadata.0.name
             }
+          }
+
+          volume_mount {
+            mount_path = "/opt/docker/conf/"
+            name       = "config"
+            read_only  = true
           }
 
           port {
@@ -115,6 +138,13 @@ resource "kubernetes_deployment" "ccloud_exporter_deployment" {
             timeout_seconds       = 30
             failure_threshold     = 3
             success_threshold     = 1
+          }
+        }
+
+        volume {
+          name = "config"
+          secret {
+            secret_name = kubernetes_secret.ccloud_exporter_config_file[0].metadata.0.name
           }
         }
       }
